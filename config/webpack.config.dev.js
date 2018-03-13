73d2a32cf624999a60abe62f5248fa8c2f3fca63
gaarson <gaarson666@gmail.com>
2018-03-13 08:34:44 +0300

    webpack config

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,10 +1,36 @@
-const path = require("path");
+process.env.BABEL_ENV = 'development';
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const 
 
 module.exports = {
-  devtool: "cheap-module-source-map",
-  entry: path.join(__dirname, "../src/index.jsx"),
+  devtool: 'cheap-module-source-map',
+  entry: path.join(__dirname, '../src/index.js'),
   output: {
-    filename: "bundle.js",
-    path: path.join(__dirname, "../public/js")
-  }
+    filename: 'bundle.js',
+    path: path.join(__dirname, '../public/js'),
+    publicPath: path.join(__dirname, '../public'),
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        loader: 'babel-loader',
+        options: {
+          presets: ['react-app'],
+        },
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+    hot: true,
+    contentBase: path.join(__dirname, '../public'),
+    headers: {'Access-control-allow-origin': '*'},
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx', 'css', '.json'],
+  },
 };
--- scripts/start.js
+++
@@ -1,4 +0,0 @@
-const webpack = require("webpack");
-const webpackDevServer = require("webpack-dev-server");
-
-const config = require("../config/webpack.config.dev");
